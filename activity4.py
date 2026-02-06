f076d23a87173f0e89f75c87b185c5975ba7bd0e
VulcanWong <wongvulcan@gmail.com>
2026-02-06 20:11:17 +0800

    lesson39

---
+++ activity4.py
@@ -0,0 +1,13 @@
+import datetime
+
+current_time = datetime.datetime.now()
+
+
+
+print("time now at greenwich merdian is :", end ="")
+print(current_time)
+
+
+
+import calendar
+print("\n", calendar.calendar(2026))
