94495222538b0d2e707a867ccb3100462e8e880f
VulcanWong <wongvulcan@gmail.com>
2026-02-19 21:08:25 +0800

    lesson39

--- homework.py
+++ homework.py
@@ -1,13 +1,14 @@
 working = float(input("Enter yourt height in cm:"))
 absent = float(input("enter your weight in kg:"))
 
-BMI = absent / (working/100)**2
+BMI = absent * (working/100)**2
 
 print("your BMI is", BMI)
 
-if BMI  < 75:
+if BMI > 75:
     print("you will not be able to sit in the chair for exam")
 elif BMI == 75:
     print("you will  be able to sit for the exam")
 else:
-    print("nice, you can sit for your exam")
+    print("nice, you can sit for your exam ")
+    print("exam time !!! The End")
